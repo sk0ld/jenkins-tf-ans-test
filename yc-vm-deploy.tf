393af3c39cc61e88090f8b190f400f1829c3fd02
Konstantin Ivanov <nejasyth@gmail.com>
2022-05-17 01:30:19 +0300

    bucket name added

--- yc-vm-deploy.tf
+++ yc-vm-deploy.tf
@@ -32,6 +32,7 @@ locals {
     image_id = "fd8sc0f4358r8pt128gg"
     zone = "ru-central1-b"
     user_name = "pcadm"
+    bucket_name = "tf-bucket-yc"
     pub_ip_vm1 = yandex_compute_instance.vm1.network_interface.0.nat_ip_address
     pub_ip_vm2 = yandex_compute_instance.vm2.network_interface.0.nat_ip_address
   }
