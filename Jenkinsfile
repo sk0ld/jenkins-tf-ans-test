963e50bbb0606f9aaacb806a436b5e8c6a8467df
Konstantin Ivanov <nejasyth@gmail.com>
2022-05-16 23:49:17 +0300

    Initial commit; testing deployment

---
+++ Jenkinsfile
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    stages {
+        stage ('Yandex Cloud infra preparation') {
+            steps {
+              sh '''
+              cp .terraformrc ~/
+              cp /home/pcadm/tech/wp.auto.tfvars . && rm -rf hosts.txt  
+              terraform init  
+              terraform plan --out=tfplan
+              terraform apply tfplan
+              '''
+            }
+        }  
+         stage ('Ansible playbook') {
+             steps {
+               sh '''
+               cp ansible.cfg ~/ 
+               ansible-playbook dev-prod-docker.yml -i hosts.txt --private-key /home/pcadm/.private_yc --user pcadm -vv
+               '''
+             }
+         }  
+
+         stage ('Yandex Cloud infra remove') {
+            steps {
+               sh '''
+               sleep 2m
+               terraform destroy --auto-approve
+               '''
+             }
+    }
+}
+
+}
---
+++ yc-vm-deploy.tf
@@ -0,0 +1,150 @@
+terraform {
+    required_providers {
+      yandex = {
+        source = "yandex-cloud/yandex"
+      }
+    }
+  }
+  
+  
+  variable "cloud_id" {
+    type = string
+    description = "Yandex Cloud id"
+  }
+  
+  variable "folder_id" {
+    type = string
+    description = "Yandex Cloud folder id"
+  }
+  
+  variable "sa_id" {
+    type = string
+    description = "Yandex Cloud service account id"
+  }
+  
+  variable "token" {
+    type = string
+    description = "Yandex Cloud token"
+  }
+  
+  
+locals {
+    image_id = "fd8sc0f4358r8pt128gg"
+    zone = "ru-central1-b"
+    user_name = "pcadm"
+    pub_ip_vm1 = yandex_compute_instance.vm1.network_interface.0.nat_ip_address
+    pub_ip_vm2 = yandex_compute_instance.vm2.network_interface.0.nat_ip_address
+  }
+  provider "yandex" {
+    token     = var.token
+    cloud_id  = var.cloud_id
+    folder_id = var.folder_id
+    zone      = local.zone
+  }
+  
+  resource "yandex_vpc_network" "network-1" {
+    name = "network1"
+  }
+  resource "yandex_vpc_subnet" "subnet-1" {
+    name           = "subnet1"
+    zone           = "ru-central1-b"
+    network_id     = yandex_vpc_network.network-1.id
+    v4_cidr_blocks = ["10.129.0.0/24"]
+  }
+  
+  resource "yandex_compute_instance" "vm1" {
+    name        = "vm-dev"
+    allow_stopping_for_update = true
+  
+    resources {
+      cores  = 2
+      memory = 2
+      core_fraction = 20
+    }
+  
+    boot_disk {
+      initialize_params {
+        image_id = local.image_id
+        type     = "network-ssd"
+        size = 15
+      }
+    }
+    network_interface {
+        subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+        nat = true
+      }
+    
+      metadata = {
+        user-data = "${file("/home/${local.user_name}/meta.txt")}"
+      }
+
+      provisioner "remote-exec" {
+        inline = [
+          "cat /etc/os-release"
+              ]
+        connection {
+          type     = "ssh"
+          user     = local.user_name
+          private_key = "${file("/home/${local.user_name}/.private_yc")}"
+          host     = self.network_interface.0.nat_ip_address
+        }
+      }
+    }  
+    
+    
+
+    resource "yandex_compute_instance" "vm2" {
+    name        = "vm-prod"
+    allow_stopping_for_update = true
+  
+    resources {
+      cores  = 2
+      memory = 2
+      core_fraction = 20
+    }
+  
+    boot_disk {
+      initialize_params {
+        image_id = local.image_id
+        type     = "network-ssd"
+        size = 15
+     }
+    }
+
+    network_interface {
+        subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
+        nat = true
+      }
+    
+      metadata = {
+        user-data = "${file("/home/${local.user_name}/meta.txt")}"
+      } 
+
+      provisioner "remote-exec" {
+        inline = [
+          "cat /etc/os-release"
+              ]
+        connection {
+          type     = "ssh"
+          user     = local.user_name
+          private_key = "${file("/home/${local.user_name}/.private_yc")}"
+          host     = self.network_interface.0.nat_ip_address
+        }
+      }
+    }  
+    
+    output "public_ip_address_vm1" {
+        value = local.pub_ip_vm1
+      }  
+
+    output "public_ip_address_vm2" {
+        value = local.pub_ip_vm2
+      }
+
+    resource "null_resource" "ansible_hosts" {
+  provisioner "local-exec" {
+          command = "echo '[dev]\n${local.pub_ip_vm1}\n[prod]\n${local.pub_ip_vm2}' > hosts.txt"
+  }
+    }
+
+                               
